f545a1e4ae514cdd2a812bc62bef371bc3fe6dbd
LilChihba <danila-yurov@mail.ru>
2023-04-22 22:07:23 +0300

    change statusBar

--- app/src/main/java/com/example/readbook/HomeActivity.kt
+++ app/src/main/java/com/example/readbook/HomeActivity.kt
@@ -4,18 +4,17 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.core.graphics.toColor
 import com.example.readbook.bottom_navigation.BottomBarNavigation
-import com.example.readbook.ui.theme.DarkGray
 import com.example.readbook.ui.theme.ReadbookTheme
 
 class HomeActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.navigationBarColor = resources.getColor(R.color.navigationBarColorMenu)
         setContent {
             ReadbookTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
@@ -25,4 +24,3 @@ class HomeActivity : ComponentActivity() {
         }
     }
 }
-
