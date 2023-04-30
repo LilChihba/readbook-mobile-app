6e20d85579c05392acd990854f7843d812168a7f
LilChihba <danila-yurov@mail.ru>
2023-04-30 18:31:52 +0300

    changed app icon and logo created profilePage

--- app/src/main/java/com/example/readbook/pages/ProfileActivity.kt
+++ app/src/main/java/com/example/readbook/pages/ProfileActivity.kt
@@ -1,7 +1,95 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.readbook.R
+import com.example.readbook.ui.theme.Blue
+import com.example.readbook.ui.theme.Milk
 
+@Preview
 @Composable
 fun ProfilePage() {
-    Text(text = "4")
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Milk)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.settings),
+                    contentDescription = "Settings",
+                    tint = Blue
+                )
+
+                Text(
+                    text = "Настройки",
+                    textAlign = TextAlign.Center,
+                    color = Blue,
+                    modifier = Modifier.padding(start = 3.dp)
+                )
+            }
+        }
+        Row(modifier = Modifier.padding(top = 15.dp)) {
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.avatar_male),
+                    contentDescription = "Avatar",
+                    modifier = Modifier.clip(RoundedCornerShape(10.dp))
+                )
+
+                Text(
+                    text = "Гость",
+                    fontSize = 26.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Blue,
+                    modifier = Modifier.padding(top = 15.dp)
+                )
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(containerColor = Blue),
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .height(45.dp)
+                        .width(275.dp)
+                ) {
+                    Text(
+                        text = "Вход или регистрация",
+                        fontSize = 17.sp
+                    )
+                }
+            }
+        }
+    }
 }
