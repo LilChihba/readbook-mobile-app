803f751b3da898c45761846fbef49201ba1d138b
LilChihba <danila-yurov@mail.ru>
2023-05-08 18:10:12 +0300

    add regPage

--- app/src/main/java/com/example/readbook/pages/AuthActivity.kt
+++ app/src/main/java/com/example/readbook/pages/AuthActivity.kt
@@ -37,7 +37,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.readbook.R
@@ -97,7 +96,7 @@ fun AuthPage(
                     color = Color.Black,
                     fontWeight = FontWeight.Bold,
                     fontSize = 26.sp,
-                    modifier = Modifier.padding(bottom = 20.dp)
+                    modifier = Modifier.padding(bottom = 30.dp)
                 )
             }
 
--- app/src/main/java/com/example/readbook/pages/RegActivity.kt
+++ app/src/main/java/com/example/readbook/pages/RegActivity.kt
@@ -1,12 +1,112 @@
 package com.example.readbook.pages
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.readbook.R
+import com.example.readbook.ui.theme.Blue
+import com.example.readbook.ui.theme.Milk
 
 @Composable
 fun RegPage(
     navigateBack: () -> Unit,
     navigateBackToProfile: () -> Unit
 ) {
-    Text(text = "RegPage")
+    val textEmail = remember{ mutableStateOf("") }
+    val textPassword = remember{ mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Milk)
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(
+                onClick = { navigateBack()},
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.arrow_back),
+                    contentDescription = "back",
+                    tint = Blue
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+            Button(
+                onClick = { navigateBackToProfile() },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                contentPadding = PaddingValues(0.dp)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.close),
+                    contentDescription = "close",
+                    tint = Blue
+                )
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 45.dp, top = 10.dp, end = 45.dp)
+        ) {
+            Row() {
+                Text(
+                    text = "Регистрация",
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 26.sp,
+                    modifier = Modifier.padding(bottom = 30.dp)
+                )
+            }
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                TextForField(text = "Почта")
+                TextBox(text = textEmail)
+
+                TextForField(text = "Пароль")
+                TextBox(text = textPassword)
+
+                Button(
+                    onClick = { /* TODO */ },
+                    colors = ButtonDefaults.buttonColors(containerColor = Blue),
+                    shape = RoundedCornerShape(7.dp),
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .height(50.dp)
+                        .fillMaxWidth()
+                ) {
+                    Text(
+                        text = "Зарегистрироваться",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 16.sp
+                    )
+                }
+            }
+        }
+    }
 }
